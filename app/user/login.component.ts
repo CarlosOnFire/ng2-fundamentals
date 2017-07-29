3bc45c93bc48fe4fdaace8701c6b97db91e57124
Carlos Barragán <carlosbarragan@iMac-de-Carlos.local>
2017-07-29 13:40:46 -0500

    Template based Form

---
+++ app/user/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+@Component({
+    templateUrl: 'app/user/login.component.html',
+
+})
+
+export class LoginComponent {
+
+    login(formValues){
+        console.log(formValues)
+    }
+
+}
--- app/user/user.routes.ts
+++ app/user/user.routes.ts
@@ -1,7 +1,9 @@
 import { ProfileComponent } from './profile.component'
+import { LoginComponent } from './login.component'
 
 export const userRoutes = [
 
-    {path: 'profile', component: ProfileComponent, canDeactivate: ['canDeactivateEditProfile']}
+    {path: 'profile', component: ProfileComponent, canDeactivate: ['canDeactivateEditProfile']},
+    {path: 'login', component: LoginComponent}
     
 ]
